969c29c0528ef47b2fe532474a5bc87a6311988c
Thomas Luu <luutoan07@gmail.com>
2022-08-20 00:31:14 +0700

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/database'; // If using Firebase database
+import 'firebase/storage';  // If using Firebase storage
+import 'firebase/firestore';   // for cloud firestore
+import 'firebase/auth';        // for authentication
+
+
+const firebaseConfig = {
+  apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
+  databaseURL: `${process.env.REACT_APP_FIREBASE_DATABASE_URL}`,
+  projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_FIREABSE_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`,
+};
+
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+const realTimeDb = app.database();
+const db = app.firestore();
+const auth = app.auth();
+const storage = firebase.storage();
+
+export { auth, db, storage, realTimeDb };
